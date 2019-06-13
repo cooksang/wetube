e86e277a2e12f8e8444e2bd1b694efbd89321c30
cooksang <cooksang@gmail.com>
2019-06-13 22:12:06 +0900

    second day

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
+import { localMiddleware } from "./middlewares";
+
+const app = express();
+app.use(helmet())
+app.set('view engine', 'pug')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(morgan("dev"))
+
+app.use(localMiddleware)
+app.use("/", globalRouter)
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
+
+export default app;
